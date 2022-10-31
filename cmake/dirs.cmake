76953b169fde3d1b7e6581b2ca523fa0b81deae5
Xarbirus <podvitskiymichael@gmail.com>
2022-10-31 22:48:23 +0300

    server project out

---
+++ cmake/dirs.cmake
@@ -0,0 +1,6 @@
+set(output_dir ${CMAKE_SOURCE_DIR}/out)
+set(output_bin ${output_dir}/bin)
+set(output_lib ${output_dir}/lib)
+
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${output_bin})
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${output_lib})
