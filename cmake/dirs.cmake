154a70d2fa7f4b4498e8dae06dae82f1f2d29220
Xarbirus <podvitskiymichael@gmail.com>
2022-11-05 11:15:06 +0300

    tests directory was changed

--- cmake/dirs.cmake
+++ cmake/dirs.cmake
@@ -1,6 +1,5 @@
 set(output_dir ${CMAKE_SOURCE_DIR}/out)
-set(output_bin ${output_dir}/bin)
-set(output_lib ${output_dir}/lib)
 
-set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${output_bin})
-set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${output_lib})
+set(CMAKE_RUNTIME_OUTPUT_DIRECTORY ${output_dir}/bin)
+set(CMAKE_LIBRARY_OUTPUT_DIRECTORY ${output_dir}/lib)
+set(CMAKE_TESTS_OUTPUT_DIRECTORY ${output_dir}/tests)
